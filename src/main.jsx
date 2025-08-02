ed4b963fd3c353c12d64db4c440d3671f375db36
maracho-byte <maracho-byte@users.noreply.github.com>
2025-08-02 18:24:56 +0000

    Fix: Restore full TODOTODO app functionality (App.jsx, main.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
+import { ThemeProvider } from './contexts/ThemeContext.jsx'
+import { NotificationProvider } from './contexts/NotificationContext.jsx'
+import { TaskProvider } from './contexts/TaskContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <NotificationProvider>
+        <TaskProvider>
+          <App />
+        </TaskProvider>
+      </NotificationProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
